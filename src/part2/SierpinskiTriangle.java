15486535af9c54d46d9fc7f1e0021445bd866c04
ethanadamsm <ethnadamsm@gmail.com>
2016-04-04 09:35:37 -0600

    Sierpinski

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -9,6 +9,8 @@ public class SierpinskiTriangle {
 	private Point2D.Double top, right, left;
 	private Color background = new Color(34, 129, 34);
 	private Color foreground = StdDraw.YELLOW;
+	private Color white = new Color(255, 255, 255);
+	private Color black = new Color(0, 0, 0);
 	
 	public SierpinskiTriangle() {
 		StdDraw.setXscale(0, 150); 
@@ -20,10 +22,21 @@ public class SierpinskiTriangle {
 		 
 		StdDraw.clear(StdDraw.GRAY); 
 	}
-   
-	public void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
+	
+	public void sierpinski(Point2D.Double top, Point2D.Double right, Point2D.Double left, int n) {
 	   // TODO: implement
-    }
+		if(n == 0){
+			return;
+		}
+		Point2D.Double lr = new Point2D.Double((left.x + right.x) / 2, right.y);
+		Point2D.Double tr = new Point2D.Double((top.x + right.x) / 2, (top.y + right.y) / 2);
+		Point2D.Double tl = new Point2D.Double((top.x + left.x) / 2, (top.y + left.y) / 2);
+		drawTriangle(tl, tr, lr, black);
+		sierpinski(top, tr, tl, n-1);
+		sierpinski(tr, right, lr, n-1);
+		sierpinski(tl, lr, left, n-1);
+		
+	}
    
 	public void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
 	   double[] x = {a.x, b.x, c.x};
@@ -33,13 +46,15 @@ public class SierpinskiTriangle {
 	}
 	
 	public void draw(int n) {
+		drawTriangle(top, right, left, white);
 		sierpinski(top, right, left, n);
+		
 	}
 	
 	
 	
 	public static void main(String[] args) { 
 		SierpinskiTriangle triangle = new SierpinskiTriangle();
-		triangle.draw(3);
+		triangle.draw(13);
 	}
 }
--- src/part3/HTree.java
+++ src/part3/HTree.java
@@ -1,23 +1,36 @@
 package part3;
+import java.awt.Color;
 import java.awt.geom.Point2D;
 import draw.StdDraw;
 
 
 public class HTree {
-	
+	private static HTree htree;
 	public static void main(String[] args) {
-		HTree htree = new HTree();
+		htree = new HTree();
 		htree.draw(3);
 	}
 
 	
 	public HTree() {
-		// TODO: implement
+		StdDraw.setXscale(0, 150); 
+		StdDraw.setYscale(0, 150); 
 		
+		StdDraw.clear(StdDraw.GRAY); 
+		fractal(htree, null, null, null, 2, Color.WHITE);
+		
+		StdDraw.show();
 	}
 	
+	private void fractal(HTree htree1, HTree htree2, HTree htree3, HTree htree4, int n, Color white) {
+		if(n == 0){
+			
+		}
+	}
+
+
 	public void draw(int n) {
-		// TODO: implement
+		
 	}
 	
 }
